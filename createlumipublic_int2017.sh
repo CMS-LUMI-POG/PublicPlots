95829a71b16b232246c0ba52561ca215db52ec3a
Paul Lujan <paul.lujan@cern.ch>
2017-10-04 16:16:07 +0200

    update script to use normtag lumi for cumulative plot; add automatic pull from git repository

--- createlumipublic_int2017.sh
+++ createlumipublic_int2017.sh
@@ -8,6 +8,11 @@ cd /afs/cern.ch/user/l/lumipro/lumiplotmachinery/internal/CMSSW_7_4_0_pre9/src/
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
 
+# Update the normtag repository. Since the cvmfs version only updates once a week,
+# we instead keep our own copy of the git repository which we can pull now!
+cd ~/Normtags
+git pull
+
 # Make the plots!
 cd ~/PublicPlots
 
@@ -19,9 +24,8 @@ cp *2017*OnlineLumi*png *2017*OnlineLumi*pdf /afs/cern.ch/cms/lumi/www/publicplo
 python create_public_lumi_plots.py public_brilcalc_plots_pp_2017_normtag.cfg
 cp *2017*NormtagLumi*png *2017*NormtagLumi*pdf /afs/cern.ch/cms/lumi/www/publicplots/
 
-# 2) Copy the cache into the cache for the all years plots. Note: internal now, soon to
-# be normtag
-cp public_lumi_plots_cache/pp_2017_online/* public_lumi_plots_cache/pp_all/
+# 2) Copy the cache into the cache for the all years plots. Note: uses normtag now!
+cp public_lumi_plots_cache/pp_2017_normtag/* public_lumi_plots_cache/pp_all/
 
 # 3) Copy cache to public location
 cp -R public_lumi_plots_cache/pp_2017_online /afs/cern.ch/user/l/lumipro/public/lumiCache/
@@ -30,8 +34,3 @@ cp -R public_lumi_plots_cache/pp_2017_normtag /afs/cern.ch/user/l/lumipro/public
 # 4) Run the all years plotting script
 python create_public_lumi_plots_allYears.py public_lumi_plots_pp_allyears.cfg
 cp peak_lumi_pp.* peak_lumi_pp_* int_lumi_cumulative_pp_2* int_lumi_cumulative_pp_1* /afs/cern.ch/cms/lumi/www/publicplots/
-
-
-
-
-

ab1689921fbec56818bf2d6f13ccd241a476e321
Paul Lujan <paul.lujan@cern.ch>
2017-11-15 16:44:18 +0100

    add config file and run script for 2017 5.02 TeV run

---
+++ createlumipublic_5TeV2017.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Overall run script for the 2017 5TeV run. This is much simpler than the
+# main 2017 script since we only need to do online luminosity, not normtag
+# or year-by-year plots.
+
+# Set up the environment. First we need to set up brilconda and then CMSSW.
+# I want to get rid of the CMSSW dependence but it doesn't quite work yet.
+
+export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.0.3/bin:$PATH
+cd /afs/cern.ch/user/l/lumipro/lumiplotmachinery/internal/CMSSW_7_4_0_pre9/src/
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scramv1 runtime -sh`
+
+# Update the normtag repository. Since the cvmfs version only updates once a week,
+# we instead keep our own copy of the git repository which we can pull now!
+cd ~/Normtags
+git pull
+
+# Make the plots!
+cd ~/PublicPlots
+
+# 1) create the plots for this year, online luminosity, and copy them to plot area
+python create_public_lumi_plots.py public_brilcalc_plots_pp5TeV_2017_online.cfg
+cp *2017*5TeV_OnlineLumi*png *2017*5TeV_OnlineLumi*pdf /afs/cern.ch/cms/lumi/www/publicplots/
+
+# 2) Copy cache to public location
+cp -R public_lumi_plots_cache/pp5TeV_2017_online /afs/cern.ch/user/l/lumipro/public/lumiCache/

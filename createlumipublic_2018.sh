213aec4ae8183dfc0047e71a77fe96eb5f94d87a
Paul Lujan <paul.lujan@cern.ch>
2018-08-10 16:08:38 +0200

    Add 2018 normtag configuration file, add it to the run script, and use it as the default for the multiyear plots

--- createlumipublic_2018.sh
+++ createlumipublic_2018.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-# Overall run script for 2018. This is basically the same as the 2017
-# run script but doesn't do normtags yet (just online).
+# Overall run script for 2018. Now that we have normtags available, this is basically identical to the 2017
+# script except for the addition of the pileup script.
 
-# Update the normtag repository. Since the cvmfs version only updates once a week,
-# we instead keep our own copy of the git repository which we can pull now! Note:
-# this now has to be done before setting up the environment since the git version
-# in CMSSW_7_4_0 is now no longer compatible.
+# Update the normtag repository. Since the cvmfs version only updates once a week, we instead keep our own
+# copy of the git repository which we can pull now! Note: this now has to be done before setting up the
+# environment since the git version in CMSSW_7_4_0 is now no longer compatible.
 cd ~/Normtags
 git pull
 
@@ -21,16 +20,25 @@ eval `scramv1 runtime -sh`
 # Make the plots!
 cd ~/PublicPlots
 
-# 1) create the plots for this year, online luminosity, and copy them to plot area
+# 1a) create the plots for this year, online luminosity, and copy them to plot area
 python create_public_lumi_plots.py public_brilcalc_plots_pp_2018_online.cfg
 cp *2018*OnlineLumi*png *2018*OnlineLumi*pdf /afs/cern.ch/cms/lumi/www/publicplots/
 
+# 1b) same, with normtag luminosity
+python create_public_lumi_plots.py public_brilcalc_plots_pp_2018_normtag.cfg
+cp *2018*NormtagLumi*png *2018*NormtagLumi*pdf /afs/cern.ch/cms/lumi/www/publicplots/
+
 # 2) Copy the cache into the cache for the all years plots. Note: uses normtag now!
-cp public_lumi_plots_cache/pp_2018_online/* public_lumi_plots_cache/pp_all/
+cp public_lumi_plots_cache/pp_2018_normtag/* public_lumi_plots_cache/pp_all/
 
 # 3) Copy cache to public location
 cp -R public_lumi_plots_cache/pp_2018_online /afs/cern.ch/user/l/lumipro/public/lumiCache/
+cp -R public_lumi_plots_cache/pp_2018_normtag /afs/cern.ch/user/l/lumipro/public/lumiCache/
 
 # 4) Run the all years plotting script
 python create_public_lumi_plots_allYears.py public_lumi_plots_pp_allyears.cfg
 cp peak_lumi_pp.* peak_lumi_pp_* int_lumi_cumulative_pp_2* int_lumi_cumulative_pp_1* /afs/cern.ch/cms/lumi/www/publicplots/
+
+# 5) Run the pileup plots script
+python create_public_pileup_plots.py public_pileup_plots_pp_2018.cfg
+cp pileup_pp_2018* /afs/cern.ch/cms/lumi/www/publicplots/

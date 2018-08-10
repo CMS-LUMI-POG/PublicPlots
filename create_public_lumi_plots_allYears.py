ecd1a34b2090a86449b776aa29e18ef612c9fcf4
Paul Lujan <paul.lujan@cern.ch>
2018-08-10 16:08:54 +0200

    fix bug with allyear plot using Hz/ub instead of Hz/nb

--- create_public_lumi_plots_allYears.py
+++ create_public_lumi_plots_allYears.py
@@ -365,6 +365,12 @@ def GetUnits(year, accel_mode, mode):
         "cum_week" : "fb^{-1}",
         "cum_year" : "fb^{-1}",
         "max_inst" : "Hz/nb",
+        },
+        2018 : {
+        "cum_day" : "fb^{-1}",
+        "cum_week" : "fb^{-1}",
+        "cum_year" : "fb^{-1}",
+        "max_inst" : "Hz/nb",
         }
         },
         "IONPHYS" : {

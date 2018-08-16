cfde1068821a12d68fd01bc47c98e0f03579c624
Paul Lujan <paul.lujan@cern.ch>
2018-08-16 15:47:24 +0200

    update brilconda version to 1.1.7 in run scripts

--- createlumipublic_2018.sh
+++ createlumipublic_2018.sh
@@ -12,7 +12,7 @@ git pull
 # Set up the environment. First we need to set up brilconda and then CMSSW.
 # I want to get rid of the CMSSW dependence but it doesn't quite work yet.
 
-export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.0.3/bin:$PATH
+export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin:$PATH
 cd /afs/cern.ch/user/l/lumipro/CMSSW_7_4_16/src/
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`

4886b789bd0334cb49cf573b75b9cb505dfc6787
Andrea Giammanco <andrea.giammanco@cern.ch>
2018-10-02 12:33:05 +0200

    force cache directories to contain the same 2018 rootfile

--- createpileuppublic_2018.sh
+++ createpileuppublic_2018.sh
@@ -17,6 +17,9 @@ cd ~/PublicPlots
 python create_public_pileup_plots.py public_pileup_plots_pp_2018.cfg --ignore-cache
 cp pileup_pp_2018* /afs/cern.ch/cms/lumi/www/publicplots/
 
+# Now make sure that the same rootfile is in both cache directories.
+cp /afs/cern.ch/user/l/lumipro/PublicPlots/public_lumi_plots_cache/pileup_2018/pileup_calc_tmp.root /afs/cern.ch/user/l/lumipro/PublicPlots/public_lumi_plots_cache/pileup_all/PileupHistogram-goldenJSON-13tev-2018.root
+
 # Create and copy the all-year pileup plots.
 python create_public_pileup_plots_allYears.py public_pileup_plots_pp_allyears.cfg
 cp pileup_allYears* /afs/cern.ch/cms/lumi/www/publicplots/

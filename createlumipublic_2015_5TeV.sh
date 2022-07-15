b0258510607a73b5c365be865c6e67ab8d0a252f
Christoph Schwick <Christoph.Schwick@cern.ch>
2022-07-15 14:02:30 +0200

    update for pp reference run 5 TeV in 2015

---
+++ createlumipublic_2015_5TeV.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Overall run script for the 2015 5TeV run. 
+
+# Update the normtag repository. Since the cvmfs version only updates once a week,
+# we instead keep our own copy of the git repository which we can pull now!
+cd ~/public/Normtags
+git pull 2>&1
+
+# Set up the environment. First we need to set up brilconda.
+export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin:$PATH
+
+# Make the plots!
+mkdir -p  /eos/user/l/lumipro/www/publicplots/2015
+cd ~/PublicPlots
+
+# 1) create the plots for this run with normtag luminosity, and copy them to plot area
+python create_public_lumi_plots.py public_brilcalc_plots_pp5TeV_2015_normtag.cfg
+#cp plots/2015/normtag/*2015*5TeV_Normtag*png plots/2015/normtag/*2015*5TeV_Normtag*pdf /eos/user/l/lumipro/www/publicplots/2015
+
+# 2) Copy cache to public location
+#cp -R -u public_lumi_plots_cache/pp5TeV_2015_normtag /afs/cern.ch/user/l/lumipro/public/lumiCache/
--- createlumipublic_5TeV2017.sh
+++ createlumipublic_5TeV2017.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
 
-# Overall run script for the 2017 5TeV run. This is much simpler than the
-# main 2017 script since we only need to do online luminosity, not normtag
-# or year-by-year plots.
+# Overall run script for the 2017 5TeV run. 
 
 # Update the normtag repository. Since the cvmfs version only updates once a week,
 # we instead keep our own copy of the git repository which we can pull now!
 cd ~/public/Normtags
 git pull 2>&1
 
-# Set up the environment. First we need to set up brilconda and then CMSSW.
-# I want to get rid of the CMSSW dependence but it doesn't quite work yet.
-
+# Set up the environment. First we need to set up brilconda.
 export PATH=$HOME/.local/bin:/afs/cern.ch/cms/lumi/brilconda-1.1.7/bin:$PATH
 
 # Make the plots!
 mkdir -p  /eos/user/l/lumipro/www/publicplots/2017
 cd ~/PublicPlots
 
-# 1) create the plots for this year, online luminosity, and copy them to plot area
-
+# 1) create the plots for this year, normtag luminosity, and copy them to plot area
 python create_public_lumi_plots.py public_brilcalc_plots_pp5TeV_2017_normtag.cfg
 cp plots/2017/normtag/*2017*5TeV_Normtag*png plots/2017/normtag/*2017*5TeV_Normtag*pdf /eos/user/l/lumipro/www/publicplots/2017/
 
